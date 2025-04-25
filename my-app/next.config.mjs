74fa2466fdbac22e58ed4ecc2fe9552976b39c34
Anisha <anishachowdhury34gmail.com>
2025-04-25 06:26:56 +0530

    gun

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require('path');
+
+const nextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      '@': path.resolve(__dirname, 'my-app'), // Update the alias to point to the 'my-app' folder
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
